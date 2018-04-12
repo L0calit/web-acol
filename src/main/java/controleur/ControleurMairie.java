3d6e8d60275d4cf1a101bf8b7952626dfd6f8e1e
Luc Boussant-roux <luc.boussant-roux@grenoble-inp.org>
2018-04-12 12:03:33 +0200

    Rajout des commentaires

--- src/main/java/controleur/ControleurMairie.java
+++ src/main/java/controleur/ControleurMairie.java
@@ -32,12 +32,26 @@ public class ControleurMairie extends HttpServlet {
     @Resource(name = "jdbc/bibliography")
     private DataSource ds;
 
-    /* pages d’erreurs */
+    /**
+     * Permet de gérer le cas si les paramètres donnés ne sont pas correctes
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
     private void invalidParameters(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/controleurErreur.jsp").forward(request, response);        
     }
 
+    /**
+     * Permet de gérer le cas où il y a eu une erreur avec la base de données
+     * @param request
+     * @param response
+     * @param e
+     * @throws ServletException
+     * @throws IOException 
+     */
     private void erreurBD(HttpServletRequest request,
                 HttpServletResponse response, DAOException e)
             throws ServletException, IOException {
@@ -47,110 +61,107 @@ public class ControleurMairie extends HttpServlet {
     }
   
     /**
-     * Actions possibles en GET : Supprimer Regime, Ajouter Regime
+     * Actions possibles en GET :
+     *  Ajouter ou supprimer un regime
+     *  Ajouter ou supprimer une activité
+     *  Ajouter ou supprimer une période
+     * @param request
+     * @param response
+     * @throws IOException
+     * @throws ServletException 
      */
+    @Override
     public void doGet(HttpServletRequest request,
             HttpServletResponse response)
             throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
         RegimeDAO regimeDAO = new RegimeDAO(ds);
         ActiviteDAO activiteDAO = new ActiviteDAO(ds);
         AccompagnateurDAO accompagnateurDAO = new AccompagnateurDAO(ds);
         PeriodeDAO periodeDAO = new PeriodeDAO(ds);
-        String action = request.getParameter("action");
-        
-        if (action.equals("regimeSupprimer")) {
-            regimeDAO.supprimerRegime(request.getParameter("regime"));
+        try {
+            if (action.equals("regimeSupprimer")) {
+                regimeDAO.supprimerRegime(request.getParameter("regime"));
+            } else if (action.equals("regimeAjouter")) {
+                actionAjouterRegime(request, regimeDAO);
+            } else if (action.equals("activiteAjouter")) {
+                actionAjouterActivite(request, activiteDAO);
+            } else if (action.equals("activiteSupprimer")) {
+                activiteDAO.supprimerActivite(request.getParameter("actiNom"), 
+                        request.getParameter("actiJour"), request.getParameter("actiHeure"));
+            } else if (action.equals("periodeSupprimer")) {
+                periodeDAO.supprimerPeriode(request.getParameter("dateDebut"), request.getParameter("dateFin"));
+            } else if (action.equals("periodeAjouter")) {
+                actionAjouterPeriode(request, periodeDAO);
+            } else if (action.equals("logout")) {
+                request.logout();
+                response.sendRedirect("index.jsp");
+            }
             actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-            
-        } else if (action.equals("regimeAjouter")) {
-            boolean requeteValide = true;
-            if (regimeDAO.existeDeja(request.getParameter("regime"))) {
-                request.setAttribute("SameRegime", "1");
-                requeteValide = false;
-                actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-            }
-            if (requeteValide) {
-                regimeDAO.ajouterRegime(request.getParameter("regime"));
-                actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-            }
-        
-        } else if (action.equals("activiteAjouter")) {
-            boolean requeteValide = true;
-            if (request.getParameter("mail1").equals(request.getParameter("mail2"))) {
-                request.setAttribute("SameAccompagnateur", "1");
-                requeteValide = false;
-                actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-            }
-            if (activiteDAO.existeDeja(request.getParameter("nom"), request.getParameter("jour"), request.getParameter("horaire"))) {
-                request.setAttribute("SameActivity", "1");
-                requeteValide = false;
-                actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-            }
-            if (requeteValide) {
-                String classes = "";
-                if (request.getParameter("PS") != null) {
-                    classes += "PS/";
-                }
-                if (request.getParameter("MS") != null) {
-                    classes += "MS/";
-                }
-                if (request.getParameter("GS") != null) {
-                    classes += "GS/";
-                }
-                if (request.getParameter("CP") != null) {
-                    classes += "CP/";
-                }
-                if (request.getParameter("CE1") != null) {
-                    classes += "CE1/";
-                }
-                if (request.getParameter("CE2") != null) {
-                    classes += "CE2/";
-                }
-                if (request.getParameter("CM1") != null) {
-                    classes += "CM1/";
-                }
-                if (request.getParameter("CM2") != null) {
-                    classes += "CM2/";
-                }
-                if (classes.equals("")) {
-                    classes = "0";
-                }
-                else {
-                    classes = classes.substring(0, classes.length()-1);
-                }
-                activiteDAO.ajouterActivite(request.getParameter("nom"), request.getParameter("jour"), request.getParameter("horaire"),
-                                         classes, Integer.parseInt(request.getParameter("prix")),
-                                          Integer.parseInt(request.getParameter("effectif")), request.getParameter("mail1"),
-                                          request.getParameter("mail2"));
-                actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-            }
-        
-        } else if (action.equals("activiteSupprimer")) {
-            activiteDAO.supprimerActivite(request.getParameter("actiNom"), request.getParameter("actiJour"), request.getParameter("actiHeure"));
-            actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-          
-        } else if (action.equals("periodeSupprimer")) {
-            periodeDAO.supprimerPeriode(request.getParameter("dateDebut"), request.getParameter("dateFin"));
-            actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-            
-        } else if (action.equals("periodeAjouter")) {
-            periodeDAO.ajouterPeriode(request.getParameter("dateDebut"), request.getParameter("dateFin"));
-            actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-        
-        } else if (action.equals("logout")) {
-            request.logout();
-            response.sendRedirect("index.jsp");
+        } catch (DAOException e) {
+            erreurBD(request, response, e);
         }
     }
     
     /**
-     * Actions possibles en POST : connexion
+     * Permet l'ajout de la période à la base de données tout en vérifiant que
+     * les données sont correctes
+     * @param request
+     * @param periodeDAO 
      */
+    public void actionAjouterPeriode(HttpServletRequest request, PeriodeDAO periodeDAO) {
+        // Verifier que la date de début est avant la date de fin 
+        // Verifier que la periode n'existe pas deja 
+        periodeDAO.ajouterPeriode(request.getParameter("dateDebut"), request.getParameter("dateFin"));
+    }
+    
+    /**
+     * Permet l'ajout d'une activité à la base de données
+     * @param request
+     * @param activiteDAO 
+     */
+    public void actionAjouterActivite(HttpServletRequest request, ActiviteDAO activiteDAO) {
+        if (request.getParameter("mail1").equals(request.getParameter("mail2"))) {
+            request.setAttribute("SameAccompagnateur", "1");
+        }
+        else if (activiteDAO.existeDeja(request.getParameter("nom"), request.getParameter("jour"), request.getParameter("horaire"))) {
+            request.setAttribute("SameActivity", "1");
+        } else  {
+            String classes = parseClass(request);
+            activiteDAO.ajouterActivite(request.getParameter("nom"), 
+                    request.getParameter("jour"), request.getParameter("horaire"),
+                    classes, Integer.parseInt(request.getParameter("prix")),
+                    Integer.parseInt(request.getParameter("effectif")), request.getParameter("mail1"),
+                    request.getParameter("mail2"));
+        }        
+    }
+    
+    /**
+     * Permet l'ajout d'un régime à la base données
+     * @param request
+     * @param regimeDAO 
+     */
+    public void actionAjouterRegime(HttpServletRequest request, RegimeDAO regimeDAO) {
+        if (regimeDAO.existeDeja(request.getParameter("regime"))) {
+            request.setAttribute("SameRegime", "1");
+        } else {
+            regimeDAO.ajouterRegime(request.getParameter("regime"));
+        }
+    }
+    
+    /**
+     * Actions possibles en POST : Connexion à son espace personelle mairie,
+     *  Création d'un compte mairie
+     * @param request
+     * @param response
+     * @throws java.io.IOException
+     * @throws javax.servlet.ServletException
+     */
+    @Override
     public void doPost(HttpServletRequest request,
             HttpServletResponse response)
             throws IOException, ServletException {
-        
         request.setCharacterEncoding("UTF-8");
         String action = request.getParameter("action");
         RegimeDAO regimeDAO = new RegimeDAO(ds);
@@ -158,43 +169,119 @@ public class ControleurMairie extends HttpServlet {
         AccompagnateurDAO accompagnateurDAO = new AccompagnateurDAO(ds);
         PeriodeDAO periodeDAO = new PeriodeDAO(ds);
         EmployeDAO employeDAO = new EmployeDAO(ds);
-        if (action == null) {
-            invalidParameters(request, response);
-            return;
-            
-        } else if (action.equals("connexion")) {
-            // tester si mdp et login corrects
-            String login = request.getParameter("login");
-            String mdp = request.getParameter("password");
-            if (employeDAO.verify(login, mdp)) {
-                request.setAttribute("login",login);
-                actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-            } else {
-                request.setAttribute("erreurLoginMairie", "1");
-                request.getRequestDispatcher("/index.jsp").forward(request, response);
+        try {
+            if (action == null) {
+                invalidParameters(request, response);
+                return;
+            } else if (action.equals("connexion")) {
+                actionConnexion(request, response, employeDAO);
+            }else if (action.equals("creationCompteMairie")) {
+                actionCreationMairie(request, response, employeDAO);
             }
-        }else if (action.equals("creationCompteMairie")) {
-            if(!request.getParameter("password1").equals(request.getParameter("password2"))){
-                request.setAttribute("differentPassword", "1");
-                request.getRequestDispatcher("/WEB-INF/mairie.jsp").forward(request, response);
-            }
-            else if(employeDAO.verifyLogin(request.getParameter("login"))){
-                request.setAttribute("loginUsed", "1");
-                request.getRequestDispatcher("/WEB-INF/mairie.jsp").forward(request, response);
-            }
-            employeDAO.creation(request.getParameter("login"), request.getParameter("password1") );
             actualiserPage(request, response, regimeDAO, activiteDAO, accompagnateurDAO, periodeDAO);
-            
+        } catch (DAOException e) {
+            erreurBD(request, response, e);
         }
-
-
     }
     
+    
+    /**
+     * Verifie que les données rentrés correspondent à un compte mairie
+     * puis affiche l'espace personnel si le login, mot de passe sont bons
+     * @param request
+     * @param response
+     * @param employeDAO
+     * @throws IOException
+     * @throws ServletException 
+     */
+    public void actionConnexion(HttpServletRequest request, HttpServletResponse response,
+            EmployeDAO employeDAO) throws IOException, ServletException{
+        String login = request.getParameter("login");
+        String mdp = request.getParameter("password");
+        if (employeDAO.verify(login, mdp)) {
+            request.setAttribute("login",login);
+        } else {
+            request.setAttribute("erreurLoginMairie", "1");
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+        }
+    }
+    
+    /**
+     * Crée un compte mairie en plus et le garde en mémoire dans la base de données
+     * @param request
+     * @param response
+     * @param employeDAO
+     * @throws IOException
+     * @throws ServletException 
+     */
+    public void actionCreationMairie(HttpServletRequest request, HttpServletResponse response,
+            EmployeDAO employeDAO) throws IOException, ServletException{
+        if(!request.getParameter("password1").equals(request.getParameter("password2"))){
+            request.setAttribute("differentPassword", "1");
+            request.getRequestDispatcher("/WEB-INF/mairie.jsp").forward(request, response);
+        }
+        else if(employeDAO.verifyLogin(request.getParameter("login"))){
+            request.setAttribute("loginUsed", "1");
+            request.getRequestDispatcher("/WEB-INF/mairie.jsp").forward(request, response);
+        }
+        employeDAO.creation(request.getParameter("login"), request.getParameter("password1"));
+    }
+    
+    /**
+     * Permet de récuperer toutes les classes choisies par l'utilisateur 
+     * @param request
+     * @return Un string avec les classes autorisés
+     */
+    public String parseClass(HttpServletRequest request) {
+        String classes = "";
+        if (request.getParameter("PS") != null) {
+            classes += "PS/";
+        }
+        if (request.getParameter("MS") != null) {
+            classes += "MS/";
+        }
+        if (request.getParameter("GS") != null) {
+            classes += "GS/";
+        }
+        if (request.getParameter("CP") != null) {
+            classes += "CP/";
+        }
+        if (request.getParameter("CE1") != null) {
+            classes += "CE1/";
+        }
+        if (request.getParameter("CE2") != null) {
+            classes += "CE2/";
+        }
+        if (request.getParameter("CM1") != null) {
+            classes += "CM1/";
+        }
+        if (request.getParameter("CM2") != null) {
+            classes += "CM2/";
+        }
+        if (classes.equals("")) {
+            classes = "0";
+        }
+        else {
+            classes = classes.substring(0, classes.length()-1);
+        }
+        return classes;
+    }
+    
+    /**
+     * Permet d'actualiser la page avec toutes les informations mises dans les paramètres
+     * @param request
+     * @param response
+     * @param regimeDAO
+     * @param activiteDAO
+     * @param accompagnateurDAO
+     * @param periodeDAO
+     * @throws IOException
+     * @throws ServletException 
+     */
     public void actualiserPage(HttpServletRequest request,
             HttpServletResponse response, RegimeDAO regimeDAO, ActiviteDAO activiteDAO, 
             AccompagnateurDAO accompagnateurDAO, PeriodeDAO periodeDAO)
             throws IOException, ServletException {
-        
         List<String> regimes = regimeDAO.getListeRegime();
         List<Activite> activites = activiteDAO.getListeActivite();
         List<String> accompagnateurs = accompagnateurDAO.getListEmail();
